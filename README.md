81957e7db8422ca4e12b0123c3e47dfa9d02a3e1
Axel Le Meur <95547308+fahrenheigt@users.noreply.github.com>
2025-01-09 11:16:04 +0100

    Init FreeCodeCamp Back End Depo

---
+++ README.md
@@ -0,0 +1,47 @@
+# Freecodecamp BackEnd Development and APIs
+
+## Description
+This project contains exercises and projects for the Freecodecamp Back-End Development and APIs certification. It covers various topics such as database management, creating RESTful APIs, and authentication.
+
+## Contents
+- [Introduction](#introduction)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [License](#license)
+
+## Introduction
+This repository contains all the projects and exercises needed to obtain the Freecodecamp Back-End Development and APIs certification.
+
+## Technologies Used
+- Node.js
+- Express.js
+- MongoDB
+- Mongoose
+- Passport.js
+
+## Installation
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/your-username/FreecodeCamp-Back-End-Certification.git
+    ```
+2. Install the dependencies:
+    ```bash
+    cd FreecodeCamp-Back-End-Certification
+    npm install
+    ```
+
+## Usage
+To start the server, run:
+```bash
+npm start
+```
+The server will be available at `http://localhost:3000`.
+
+## Contributions
+Contributions are welcome! Please submit a pull request for any improvements or bug fixes.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
---
+++ boilerplate-express-main/myApp.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+// #1
+// console.log("Hello World");
+
+// #2
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// });
+
+// #3
+app.get("/", (req, res) => {
+    absolutePath = __dirname + "/views/index.html";
+    res.sendFile
+    (absolutePath);
+}
+);
+
+// #7
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
+  });
+
+// #4
+app.use("/public", express.static(__dirname + "/public"));
+
+// #11
+app.use(bodyParser.urlencoded({extended: false}));
+
+// #5
+//app.get("/json", (req, res) => {
+//    res.json({ message: "Hello json" });
+//});
+
+// #6
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({ message: "HELLO JSON" });
+    } else {
+        res.json({ message: "Hello json" });
+    }
+}
+);
+
+// #8
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  }, (req, res) => {
+    res.send({ time: req.time });
+  });
+
+// #9
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params.word });
+  }
+);
+
+// #10
+app.get("/name", (req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  }
+);
+
+// #12
+app.post("/name", (req, res) => {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
